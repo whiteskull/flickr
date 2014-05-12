047377f66f82fd80d7d77b74570851f7a7ed48e0
Sergey Belotelkin <sergey.belotelkin@gmail.com>
2014-05-12 19:48:43 +0400

    some changes

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -2,14 +2,13 @@ class FlickrController < ApplicationController
   require 'open-uri'
 
   def search
-
     # Tags for search
     tags = URI.escape(params[:q].mb_chars.downcase.to_s.gsub(' ', ','))
 
     # Flickr url
     url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{FLICKR_API_KEY}&tags=#{tags}&safe_search=1&per_page=20"
 
-    # Get xml flickr photo
+    # Get xml flickr photos
     xml = Rails.cache.fetch("flickr_search_#{tags}") { open(url).read }
 
     photos = Nokogiri::XML(xml)
