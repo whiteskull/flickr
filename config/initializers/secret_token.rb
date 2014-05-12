09893e06b2250adbfdf4761a7941c0c32b819ca5
Sergey Belotelkin <sergey.belotelkin@gmail.com>
2014-05-12 15:55:08 +0400

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flickr::Application.config.secret_key_base = '78c702fe8e6b6ba100fc4efe36d4375689e177626caee4aeeb1371417b877906deac4e40a05a2ce6dfa935bae57673c00ea1aea95345227612f53af97d83a0ef'
